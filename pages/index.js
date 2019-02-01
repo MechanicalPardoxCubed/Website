8c86e8c8824ede6bd423f884702cca527900933e
havish10 <netlahavish@gmail.com>
2019-02-01 14:39:39 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>Mechanical Paradox Cubed</title>
+      <link href="/static/styles.css" rel="stylesheet" />
+    </Head>
+    <h1 className="title">Mechanical Paradox Cubed</h1>
+    <div className="hero">
+      {/* <img
+        src="/static/drawing-1.svg"
+        alt="Mechanical Paradox Cubed Logo"
+        className="hero"
+      /> */}
+    </div>
+  </div>
+)
